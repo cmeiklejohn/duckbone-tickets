258816f5a2a157c6b28fab26378c1c3b54a14044
Max Lord <maxlord@gmail.com>
2011-11-28 19:35:31 -0500

    Docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 DuckboneTickets::Application.routes.draw do
 
+  # JSON
   resources :tickets, :only => [:index, :show, :create, :update, :destroy] do
     resources :comments, :only => [:create, :destroy]
   end
-
   resources :users, :only => [:index, :show, :create, :update, :destroy]
 
+  # HTML
   root :to => 'home#index'
 
 end
